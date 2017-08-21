76b50506f57ac8cec5e16682e05b577a09968ead
Tahir Naushasd <t.naushad25@gmail.com>
2017-08-21 10:21:04 +0100

    asp.net core 2 middleware

---
+++ Fiver.Asp.Middleware/RunMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Fiver.Asp.HelloMiddleware
+{
+    public static class RunMiddlewareExtensions
+    {
+        public static void RunHelloWorld(this IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World! (Run in Extension Method)");
+            });
+        }
+    }
+}
---
+++ Fiver.Asp.Middleware/UseMiddlewareExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Fiver.Asp.HelloMiddleware
+{
+    public static class UseMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseHelloWorld(this IApplicationBuilder app)
+        {
+            return app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello World! (Use)\n");
+                await next();
+            });
+        }
+
+        public static IApplicationBuilder UseHelloWorldInClass(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<HelloWorldMiddleware>();
+        }
+    }
+
+    public class HelloWorldMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public HelloWorldMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await context.Response.WriteAsync("Hello World! (Use in Class)\n");
+            await this.next(context);
+        }
+    }
+}
